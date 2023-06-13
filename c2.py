39771fc19d5bfd8fd7317fa5cc5a17fced7aa752
dionyg <89809538+dionyg@users.noreply.github.com>
2023-06-12 22:11:01 -0400

    Add files via upload
    Servidor y cliente, versón inicial funcional donde cada uno recibe y envía mensajes sin problema dentro del mismo dispositivo

---
+++ c2.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+direccion_servidor = ('localhost', 55555)
+
+def receive_messages(cliente):
+    while True:
+        datos = cliente.recv(1024)
+        if datos:
+            print(datos.decode())
+        else:
+            cliente.close()
+            break
+
+def start_client():
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect(direccion_servidor)
+
+    receive_thread = threading.Thread(target=receive_messages, args=(cliente,))
+    receive_thread.start()
+
+    while True:
+        message = input('->')
+        cliente.send(message.encode())
+
+start_client()
